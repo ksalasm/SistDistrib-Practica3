5b97dbc6a63feff0014dfb3383c523fb6830a1b9
Karla Salas <ksalasm17@gmail.com>
2012-11-23 09:26:57 -0600

    3
    3

---
+++ tutorial3/1compileService.bat
@@ -0,0 +1,1 @@
+javac -cp "universidad/WEB-INF/classes";"universidad/WEB-INF/lib/*" -d universidad/WEB-INF/classes src/service/beans/Grupo.java src/service/beans/Profesor.java src/service/storage/ProfesorStore.java src/service/storage/GrupoStore.java src/service/resources/ProfesorResource.java src/service/resources/ProfesoresResource.java src/service/util/ParamUtil.java
